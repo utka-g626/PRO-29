88f10b17702ab4bdf2678cb1d95976a54935c5ce
utka-g626 <77826477+utka-g626@users.noreply.github.com>
2021-01-29 12:57:57 +0530

    Add files via upload

---
+++ Projectile.js
@@ -0,0 +1,30 @@
+class Projectile{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:4,
+        }
+        this.pointB = pointB;
+        this.body = Constraint.create(options);
+        World.add(world,this.body);
+    }
+    attach(body){
+        this.projectile.bodyA=body
+            } 
+
+    release(){
+        this.body.bodyA = null;  
+    }
+
+    display(){
+        if(this.body.bodyA){
+            var pointA = this.body.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(1);
+            stroke("grey");
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+}
